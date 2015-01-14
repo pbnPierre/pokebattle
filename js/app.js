d86e8e0144e3907535e56729e288270ff7d898be
PbnPierre <pbertran@prismamedia.com>
2015-01-14 14:12:30 +0100

    Add pokeapi support and pokemon search

--- js/app.js
+++ js/app.js
@@ -1,19 +1,57 @@
+String.prototype.ucfirst = function()
+{
+    return this.charAt(0).toUpperCase() + this.substr(1);
+};
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find a été appelé sur null ou undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate doit être une fonction');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
 var PokebattleApp = angular.module('Pokebattle.app', ['ngRoute']);
 
 PokebattleApp.config(function ($routeProvider) {
-    $routeProvider.when('/',           {templateUrl: 'view/Pokebattle.html',    controller: 'WeaknessAndStrengthController'});
+    $routeProvider.when(
+        '/',
+        {templateUrl: 'view/Pokebattle.html',    controller: 'WeaknessAndStrengthController'}
+    );
     $routeProvider.otherwise({redirectTo: '/'});
 });
 
-PokebattleApp.run(function ($rootScope) {});
+PokebattleApp.controller('WeaknessAndStrengthController', ['$scope', function ($scope)
+{
+    $scope.types = {
+        primary: undefined,
+        secondary: undefined
+    };
+    $scope.elements = elementDefinitions;
 
-PokebattleApp.controller('WeaknessAndStrengthController', ['$scope', function ($scope) {
     $scope.calculate = function(){
         $scope.initialize();
+
         if ($scope.types.primary != undefined)
         {
             $scope.calculateElements($scope.types.primary, $scope.types.secondary);
         }
+
+        $scope.$apply();
     };
 
     $scope.selectPrimary = function(name)
@@ -36,10 +74,6 @@ PokebattleApp.controller('WeaknessAndStrengthController', ['$scope', function ($
         $scope.types.secondary = undefined;
         $scope.calculate();
     };
-
-    $scope.print = function() {
-        window.print();
-    }
 
     $scope.initialize = function(){
         $scope.effects = {
@@ -92,11 +126,52 @@ PokebattleApp.controller('WeaknessAndStrengthController', ['$scope', function ($
         }
     }
 
-    $scope.types = {
-        primary: undefined,
-        secondary: undefined
-    };
+    $scope.calculateFromPokemonName = function(){
+        if ($scope.pokemon !== undefined)
+        {
+            var apiBase = 'http://pokeapi.co/';
 
-    $scope.elements = elementDefinitions;
-    $scope.calculate();
+            jQuery.getJSON(
+                apiBase + 'api/v1/pokedex/1/',
+                function(data)
+                {
+                    var sortFunction = function(element, index, array){
+                        return (element.name == $scope.pokemon.toLowerCase());
+                    };
+
+                    var pokemon = data.pokemon.find(sortFunction);
+                    if (pokemon != undefined)
+                    {
+                        var ressourceUrl = apiBase + pokemon.resource_uri;
+
+                        jQuery.getJSON(
+                            ressourceUrl,
+                            function(data){
+                                $scope.selectPrimary(data.types[0].name.ucfirst());
+
+                                if (data.types[1] != undefined)
+                                {
+                                    $scope.selectSecondary(data.types[1].name.ucfirst());
+                                }
+                                else
+                                {
+                                    $scope.removeSecondary();
+                                }
+                            }
+                        ).error(function(){
+                            alert('Pokeapi is not available');
+                        });
+                    }
+                    else
+                    {
+                        alert( $scope.pokemon + ' is not a real pokemon');
+                    }
+                }
+            ).error(function(){
+                alert('Pokeapi is not available');
+            });
+        }
+    };
 }]);
+
+PokebattleApp.run();
